2926c870fd4d2a8b7433dc4dd5b82737df51ed3b
Patrick Behrens <thebeagle@Patricks-MacBook-Pro.local>
2013-05-12 20:47:54 -0500

    one more commit

--- wluw/radio/frontend/urls.py
+++ wluw/radio/frontend/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('radio.frontend.views',
     url(r'^$', 'home', name='home'),
+	# (r'^about', 'django.views.generic.simple.direct_to_template', {'template': 'frontend/template/about.html'}),
+
 )
